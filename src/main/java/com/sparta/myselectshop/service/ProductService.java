0513479751c874a2c066a6c93fb34e26dc65414c
euneuneun <daeun2597@gmail.com>
2026-04-15 01:50:00 +0900

    관심 상품 조회 API

--- src/main/java/com/sparta/myselectshop/service/ProductService.java
+++ src/main/java/com/sparta/myselectshop/service/ProductService.java
@@ -9,6 +9,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class ProductService {
@@ -36,6 +39,18 @@ public class ProductService {
         product.update(requestDto);
 
         return new ProductResponseDto(product);
+
+    }
+
+    public List<ProductResponseDto> getProducts() {
+        List<Product> productList = productRepository.findAll();
+        List<ProductResponseDto> responseDtosList = new ArrayList<>();
+
+        for (Product product : productList){
+            responseDtosList.add(new ProductResponseDto(product));
+        }
+
+        return responseDtosList;
 
     }
 }
